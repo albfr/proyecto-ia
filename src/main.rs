cdb95db65ae8197e9ce724c90237c6ec2032ab1d
Leonardo Lovera <leonard.lover@gmail.com>
2025-02-10 13:44:48 -0300

    feat: implemented DLX

--- src/main.rs
+++ src/main.rs
@@ -43,6 +43,10 @@ impl Record {
     fn add_node(&mut self) {
         self.length += 1;
     }
+
+    fn remove_node(&mut self) {
+        self.length -= 1;
+    }
 }
 
 #[derive(Debug)]
@@ -83,6 +87,76 @@ impl Node {
 
     fn set_down(&mut self, down: usize) {
         self.down= down;
+    }
+}
+
+fn hide(p: usize, item_header: &mut [Record], node_list: &mut [Node]) {
+    let mut q = p + 1;
+
+    while q != p {
+        let node = &node_list[q];
+        let x = node.top;
+        let u = node.up;
+        let d = node.down;
+
+        if x <= 0 {
+            q = u;
+        } else {
+            node_list[u].set_down(d);
+            node_list[d].set_up(u);
+            item_header[x as usize].remove_node();
+            q += 1;
+        }
+    }
+}
+
+fn unhide(p: usize, item_header: &mut [Record], node_list: &mut [Node]) {
+    let mut q = p - 1;
+
+    while q != p {
+        let node = &node_list[q];
+        let x = node.top;
+        let u = node.up;
+        let d = node.down;
+
+        if x <= 0 {
+            q = d;
+        } else {
+            node_list[u].set_down(q);
+            node_list[d].set_up(q);
+            item_header[x as usize].add_node();
+            q -= 1;
+        }
+    }
+}
+
+fn cover(i: usize, item_header: &mut [Record], node_list: &mut [Node]) {
+    let mut p = node_list[i].down;
+
+    while p != i {
+        hide(p, item_header, node_list);
+        p = node_list[p].down; 
+    }
+
+    let l = item_header[i].left;
+    let r = item_header[i].right;
+
+    item_header[l].set_right(r);
+    item_header[r].set_left(l);
+}
+
+fn uncover(i: usize, item_header: &mut [Record], node_list: &mut [Node]) {
+    let l = item_header[i].left;
+    let r = item_header[i].right;
+
+    item_header[l].set_right(i);
+    item_header[r].set_left(i);
+
+    let mut p = node_list[i].up;
+
+    while p != i {
+        unhide(p, item_header, node_list);
+        p = node_list[p].up; 
     }
 }
 
@@ -199,7 +273,7 @@ fn main() {
                 option_items.insert(i);
 
                 let u = node_list[i].up;
-                let j = node_list.len();
+                let j = node_list.len() as usize;
 
                 item_header[i].add_node();
                 node_list.push(Node::new(i as isize, u, i));
@@ -217,5 +291,103 @@ fn main() {
         spacer = s;
 
         option_buffer.clear();
+    };
+
+    let mut level = 0;
+    let mut solution = Vec::new();
+    let mut exit_level = false;
+
+    loop {
+        let check_exit = exit_level;
+        exit_level = false;
+
+        if item_header[0].right != 0 && !check_exit {
+            let mut min_length = usize::MAX;
+            let mut p = item_header[0].right;
+            i = p;
+
+            while p != 0 {
+                let length = item_header[p].length;
+
+                if length < min_length {
+                    min_length = length;
+                    i = p;
+
+                    if min_length == 0 {
+                        break;
+                    }
+                }
+
+                p = item_header[p].right;
+            }
+
+            cover(i, &mut item_header, &mut node_list);
+
+            if level == solution.len() {
+                solution.push(node_list[i].down);
+            } else if level < solution.len() {
+                solution[level] = node_list[i].down;
+            } else {
+                panic!("Cannot skip levels in solution.");
+            }
+        } else {
+            if !check_exit {
+                println!("Visiting solution...");
+                for j in 0..level {
+                    let mut r = solution[j];
+                    while node_list[r].top >= 0 {
+                        r += 1;
+                    }
+
+                    r = node_list[r].up;
+                    let position = item_header[node_list[r].top as usize].name.clone().unwrap();
+                    let digit = item_header[node_list[r + 1].top as usize].name.clone().unwrap().chars().last().unwrap();
+
+                    println!("{} {}", position, digit);
+                }
+
+                println!("Finished visiting solution!");
+            }
+
+            if level == 0 {
+                break;
+            }
+
+            level -= 1;
+
+            let mut p = solution[level] - 1;
+
+            while p != solution[level] {
+                let j = node_list[p].top;
+                if j <= 0 {
+                    p = node_list[p].down;
+                } else {
+                    uncover(j as usize, &mut item_header, &mut node_list);
+                    p -= 1;
+                }
+            }
+
+            i = node_list[solution[level]].top as usize;
+            solution[level] = node_list[solution[level]].down;
+        }
+
+        if solution[level] == i {
+            uncover(i, &mut item_header, &mut node_list);
+            exit_level = true;
+        } else {
+            let mut p = solution[level] + 1;
+
+            while p != solution[level] {
+                let j = node_list[p].top;
+                if j <= 0 {
+                    p = node_list[p].up;
+                } else {
+                    cover(j as usize, &mut item_header, &mut node_list);
+                    p += 1;
+                }
+            }
+
+            level += 1;
+        }
     };
 }
